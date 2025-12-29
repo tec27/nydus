54c54c4a4447a9787134b616363ef9f3f61a7869
Travis Collins <travis@tec27.com>
2025-12-28 21:01:08 -0800

    Fix unused generic warning.

--- typings/webpack.d.ts
+++ typings/webpack.d.ts
@@ -2,6 +2,7 @@
 declare module 'webpack' {
   export type Compilation = any
   export type Compiler = any
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   export type LoaderContext<T> = any
   export type WebpackPluginInstance = any
 }
